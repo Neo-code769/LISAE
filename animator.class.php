da3158767bb75dc5a16dac43c207b9ac890e290d
Neo-code769 <56584577+Neo-code769@users.noreply.github.com>
2020-05-25 11:26:59 +0200

    new animator class

---
+++ animator.class.php
@@ -0,0 +1,31 @@
+<?php
+
+    class Animator extends User {
+        
+        public function createSlot() {
+            //TODO
+        }
+
+        public function deleteSlot() {
+            //TODO
+        }
+
+        public function changeSlot() {
+            //TODO
+        }
+
+        public function checkSlot() {
+            //TODO
+        }
+
+        public function getRegistered() {
+            //TODO
+        }
+
+        public function generatePresentList() {
+            //TODO
+        }
+
+    }
+
+?>
