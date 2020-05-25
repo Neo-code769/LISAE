fe8a8b004bdea5d7bbc0ec949b51147c30e256be
Nale974 <58567101+Nale974@users.noreply.github.com>
2020-05-25 11:09:42 +0200

    new class Activity

---
+++ Activity.class.php
@@ -0,0 +1,24 @@
+<?php
+class Activity{
+    private $_name;
+    private $_description;
+    private $_detailedDescription;
+    private $_minNumberPerson;
+    private $_maxNumberPerson;
+    private $_image;
+    private $_registrationDeadline;
+    private $_unsubscribeDeadline;
+    private $_slot = [];
+
+    function __construct($name, $description, $detailedDescription, $minNumberPerson, $maxNumberPerson, $registrationDeadline, $unsubscribeDeadline)
+    {
+        $this->_name=$name;
+        $this->_description=$description;
+        $this->_detailedDescription=$detailedDescription;
+        $this->_minNumberPerson=$minNumberPerson;
+        $this->_maxNumberPerson=$maxNumberPerson;
+        $this->_image=null;
+        $this->_registrationDeadline=$registrationDeadline;
+        $this->_unsubscribeDeadline=$unsubscribeDeadline;
+    }
+}
