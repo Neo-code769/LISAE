3e146ac2f060c289fbb888f8e17d313bd32ebcc8
Nale974 <58567101+Nale974@users.noreply.github.com>
2020-05-25 11:49:29 +0200

    Create controller.class.php

---
+++ controller/controller.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class Controller{
+
+    public function run()
+    {
+        //TODO TEST
+        //$act1 = new Activity("Atelier CV", "hey", "heyyyy", 2, 3, "3 jours avant", "2 jours avant");
+        //echo($act1->getName());
+    }
+
+}
