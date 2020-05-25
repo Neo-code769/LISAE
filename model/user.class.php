c5b23ef65fa3e6f8aa21ce013043decab3546018
Neo-code769 <56584577+Neo-code769@users.noreply.github.com>
2020-05-25 11:45:43 +0200

    commit in files model for all users class

---
+++ model/user.class.php
@@ -0,0 +1,31 @@
+<?php
+
+    class User {
+
+        private $_lastname;
+        private $_firstname;
+        private $_birthdate;
+        private $_phoneNumber;
+        private $_mail;
+        private $_password;
+
+        public function __construct($lastname, $firstname, $birthdate, $phoneNumber, $mail, $password) {
+            $this->_lastname = $lastname;
+            $this->_firstname = $firstname;
+            $this->_birthdate = $birthdate;
+            $this->_phoneNumber = $phoneNumber;
+            $this->_mail = $mail;
+            $this->_password = $password;
+        }
+
+        public function getAllActivity() {
+            //TODO
+        }
+
+        public function getListActivity() {
+            // TODO
+        }
+
+    }
+
+?>
