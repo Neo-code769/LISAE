381044d27634264eafb92f5e9dcd163b39336517
NeoCode <pierre.trublereau@gmail.com>
2020-05-26 16:15:27 +0200

    maj header&footer + img

--- view/header.php
+++ view/header.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
     <html>
     <head>
-        <title>LISAE</title>
+        <meta charset="UTF-8">
+        <meta name="LISAE" content="SoftWare Manage Registration to Activity ELOCE AFPA">
+        <meta name="author" content="Emma SCHURRER, Nathan LEBON, Pierre TRUBLEREAU">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="mystyle.css">
+        <title>LISAE - ELOCE</title>
     </head>
+
+    <header>
+        <! -- TODO -- >
+    </header>
 
     <body>
