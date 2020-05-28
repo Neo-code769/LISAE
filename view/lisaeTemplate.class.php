787755719b1c8334ed962895ff5f34eff382e6bb
NeoCode <pierre.trublereau@gmail.com>
2020-05-28 16:34:29 +0200

    Bootstrap + CSS maj

--- view/lisaeTemplate.class.php
+++ view/lisaeTemplate.class.php
@@ -15,7 +15,7 @@ abstract class LisaeTemplate {
                     <meta name="author" content="Emma SCHURRER, Nathan LEBON, Pierre TRUBLEREAU">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <link rel="icon" type="image/x-icon" href="./images/favicon.ico" />
-                    <link rel="stylesheet" href="/view/front-end/style.css" >
+                    <link rel="stylesheet" href="view/Front-end/style.css" >
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
                     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -30,9 +30,7 @@ abstract class LisaeTemplate {
             <footer> 
                 <figure>
                 <img src="images/AFPA.jpg" alt="logo AFPA" />
-                </figure>
-        
-                <figure>
+                <img src="images/ELOCE.png" alt="logo ELOCE" />
                 <img src="images/LISAE.png" alt="logo LISAE" />
                 </figure>
             </footer>
--- view/loginPage.php
+++ view/loginPage.php
@@ -1,19 +1,26 @@
 
     <body>
-        <h2>Login Page</h2>
+
+        <div id="title" class="container">
+            <div class="row">
+                <div class="col-xl-4">
+                    <h2>Login Page</h2>
+                </div>
+            </div>
+        </div>
 
         <div id="connexion" class="row">
-            <div class="col-md-4">
+            <div class="col-xl-4">
             <fieldset>
                 <form action="./index.php/collab/checkConnection" method="post">
                     <label for="email">E-mail:</label><br>
-                    <input type="email" id="email" name="mail"><br>
+                    <input type="email" id="email" name="mail"><br><br>
                     <label for="mdp">Mot de passe:</label><br>
                     <input type="password" id="mdp" name="password"><br><br>
                     <input type="submit" value="Connexion">
                 </form><br>
 
-                    <button value="Inscription"><a href="./index.php/collab/registration"> Inscription </a></button>
+                    <button id="inscription" value="Inscription"><a href="./index.php/collab/registration"> Inscription </a></button>
 
             </fieldset>   
             </div> 
