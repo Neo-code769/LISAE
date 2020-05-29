ef3f89b21b4a769b21989f3a92a46e767b145ca1
NeoCode <pierre.trublereau@gmail.com>
2020-05-29 10:42:19 +0200

    lien images fixed

--- view/lisaeTemplate.class.php
+++ view/lisaeTemplate.class.php
@@ -21,7 +21,7 @@ abstract class LisaeTemplate {
                     <meta name="author" content="Emma SCHURRER, Nathan LEBON, Pierre TRUBLEREAU">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <link rel="icon" type="image/x-icon" href="./images/favicon.ico" />
-                    <link rel="stylesheet" href="view/Front-end/style.css" >
+                    <link rel="stylesheet" href="/LISAE/view/Front-end/style.css" >
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
                     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -37,9 +37,9 @@ abstract class LisaeTemplate {
         echo <<<EOD
             <footer> 
                 <figure>
-                    <img src="images/AFPA.jpg" alt="logo AFPA" />
-                    <img src="images/ELOCE.png" alt="logo ELOCE" />
-                    <img src="images/LISAE.png" alt="logo LISAE" />
+                    <img src="/LISAE/images/AFPA.jpg" alt="logo AFPA" />
+                    <img src="/LISAE/images/ELOCE.png" alt="logo ELOCE" />
+                    <img src="/LISAE/images/LISAE.png" alt="logo LISAE" />
                 </figure>
             </footer>
         </body>
