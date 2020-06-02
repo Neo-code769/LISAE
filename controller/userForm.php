33f90fee614bf2a5df13c3ff36b0d1ef6d9560e9
NeoCode <pierre.trublereau@gmail.com>
2020-06-02 20:26:43 +0200

    Integration PHPMailer into userForm.php

--- controller/userForm.php
+++ controller/userForm.php
@@ -23,6 +23,7 @@ class UserForm{
 
     }
 
+    // Verification de la concordance des mots de passe //
     private function checkPassword()
     {
         $passwordOk = false;
@@ -33,6 +34,7 @@ class UserForm{
         return $passwordOk;
     }
     
+    // Verification de l'inexistance d'un compte similaire mail existant //
     private function checkMail()
     {
         $mailOk = false;
@@ -47,11 +49,60 @@ class UserForm{
         return $mailOk;
     }
 
-    public function createAnimator()
+    // Verification de la confirmation du compte mail //
+    private function checkConfirmation()
     {
-        $anim = null;
-        if ($this->checkPassword() && $this->checkMail() ) {
-            $anim = new Animator($this->_lastname,$this->_firstname,$this->_birthdate,$this->_phoneNumber,$this->_mail,$this->_password);
+        $confirmMail = false;
+        $confirmOK = false
+        $userDao = new UserDao();
+        // recup $_confirmMail
+            if ($tab['exist'] == 1) 
+            {
+                $_confirmMail = true;
+                return $confirmMail;
+            }else {
+                echo 'Veuillez confirmer votre adresse e-mail! <a>' . $this->sendMailConfirmation(); . 'Cliquez sur ce lien pour renvoyer un e-mail de confirmation </a>';
+            }
+    }
+
+    /////// Verification e-mail PHPMailer /////////
+    private function sendMailConfirmation() 
+    {
+        $mail= new PHPMailer();
+ 
+        $mail->isSMTP(); // Paramétrer le Mailer pour utiliser SMTP 
+        $mail->Host = 'smtp.gmail.com'; // Spécifier le serveur SMTP
+        $mail->SMTPAuth = true; // Activer authentication SMTP
+        $mail->Username = 'contact.afpa.lisae@gmail.com'; // Votre adresse email d'envoi
+        $mail->Password = 'AR3n96f4aQ'; // Le mot de passe de cette adresse email
+        $mail->SMTPSecure = 'ssl'; // Accepter SSL
+        $mail->Port = 465; 
+
+        $mail->setFrom('contact.afpa.lisae@gmail.com', 'AFPA LISAE');
+        $mail->addAddress('pierre.trublereau@gmail.com', 'Pierre Trublereau'); /****** TODO: Personnaliser l'adresse d'envoi **********/
+        $mail->addReplyTo('contact.afpa.lisae@gmail.com', 'Information'); // L'adresse de réponse
+        $mail->Subject = 'Confirmation de votre Mail - AFPA-LISAE';
+        $mail->Body = 'Veuillez confirmer votre adresse en mail en cliquant sur ce lien: <br>
+        <a href="http://www.lisae.fr/confirm-registration"> Cliquer ici pour confirmer votre mail ! </a>'; // Creation page: "LISAE/registration/confirm-registration"
+        $mail->isHTML(true);
+        $mail->setLanguage('fr');
+
+        if ($mail->send()) {
+            echo 'Confirmation Message has been sent.';
+        }else {
+            echo 'Message was not sent.<br>';
+            echo 'Mailer error: ' . $mail->ErrorInfo; 
+        }
+    }
+
+    ///////// Creation Collaborateur /////////////
+    public function createCollab()
+    {
+        $collab = null;
+
+        if ($this->checkPassword() && $this->checkMail()) {
+            $collab = new Collaborator($this->_lastname,$this->_firstname,$this->_birthdate,$this->_phoneNumber,$this->_mail,$this->_password);
+            $this->sendMailConfirmation();  // Envoi du mail de confirmation
         }elseif($this->checkPassword() == false){
             throw new LisaeException("Erreur, les mots de passe ne correspondent pas !");
         }elseif($this->checkMail() == false){
@@ -59,9 +110,27 @@ class UserForm{
         }else {
             throw new LisaeException("Erreur, les mots de passe ne correspondent pas et/ou le mail est déjà utilisé !");
         }   
-        return $anim;
+        return $collab;
     }
+}    
 
+    ///////// Creation Animateur /////////////
+    public function createAnimator()
+        {
+            $anim = null;
+            if ($this->checkPassword() && $this->checkMail() ) {
+                $anim = new Animator($this->_lastname,$this->_firstname,$this->_birthdate,$this->_phoneNumber,$this->_mail,$this->_password);
+            }elseif($this->checkPassword() == false){
+                throw new LisaeException("Erreur, les mots de passe ne correspondent pas !");
+            }elseif($this->checkMail() == false){
+                throw new LisaeException("Erreur, le mail est déjà utilisé !");
+            }else {
+                throw new LisaeException("Erreur, les mots de passe ne correspondent pas et/ou le mail est déjà utilisé !");
+            }   
+            return $anim;
+        }
+
+    ///////// Creation Administrateur /////////////
     public function createAdministrator()
     {
         $admin = null;
@@ -76,19 +145,3 @@ class UserForm{
         }   
         return $admin;
     }
-
-    public function createCollab()
-    {
-        $collab = null;
-        if ($this->checkPassword() && $this->checkMail() ) {
-            $collab = new Collaborator($this->_lastname,$this->_firstname,$this->_birthdate,$this->_phoneNumber,$this->_mail,$this->_password);
-        }elseif($this->checkPassword() == false){
-            throw new LisaeException("Erreur, les mots de passe ne correspondent pas !");
-        }elseif($this->checkMail() == false){
-            throw new LisaeException("Erreur, le mail est déjà utilisé !");
-        }else {
-            throw new LisaeException("Erreur, les mots de passe ne correspondent pas et/ou le mail est déjà utilisé !");
-        }   
-        return $collab;
-    }
-}
--- index.php
+++ index.php
@@ -44,41 +44,9 @@ require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 require_once 'vendor/autoload.php'; 
 require_once 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
 
+/********************************** */
 /********** CODE PRINCIPAL **********/
-
-/*(new CollabView())->run('dashboard');*/ // Test Template CollabView
-
-// Création d'une instance de notre programme et du moteur SVG puis exécution.
-
-////////////////////////////////////////////////
-// Test PHPMailer
-
-$mail= new PHPMailer();
-
-/* 
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-$mail->isSMTP(); // Paramétrer le Mailer pour utiliser SMTP 
-$mail->Host = 'mail.LISAE.fr'; // Spécifier le serveur SMTP
-$mail->SMTPAuth = true; // Activer authentication SMTP
-$mail->Username = 'toto@gmail.com'; // Votre adresse email d'envoi
-$mail->Password = 'mdp'; // Le mot de passe de cette adresse email
-$mail->SMTPSecure = 'ssl'; // Accepter SSL
-$mail->Port = 465; */
-
-$mail->setFrom('Stone-82@hotmail.fr', 'Pierre Trublereau');
-$mail->addAddress('pierre.trublereau@gmail.com', 'Pierre Trublereau');
-$mail->Subject = 'Confirmation Mail';
-$mail->Body = 'Hi! This is my first e-mail sent through PHPMailer.';
-$mail->isHTML(true);
-$mail->setLanguage('fr', '/optional/path/to/language/directory/');
-if(!$mail->send()) {
-  echo 'Message was not sent.<br>';
-  echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent.';
-}
-
-//////////////////////////////////////////
+/********************************** */
 
 try {
 	$controllerName = (new MainController())->getClassName();
