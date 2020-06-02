370abcf01621e101048f09819b8a9f51a4137aed
EmmaS <emmaschurrer@msn.com>
2020-06-02 16:15:55 +0200

    modif sessionDao

--- dao/sessionDao.class.php
+++ dao/sessionDao.class.php
@@ -22,19 +22,32 @@ class userDao extends Dao{
 
     }
 
-    public function getSessionList() : void 
+    public function getSessionList() : array 
     {
-        $sql = "SELECT `host.slotDate`, `activity.name`, `host.slotHour` 
+        $list = []; 
+        $pdo = Dao::getConnexion();
+        $requete = $sql->prepare("SELECT `host.slotDate`, `activity.name`, `host.slotHour` 
                 FROM `session` INNER JOIN `tie` ON `tie.id_session` = `session.id_session`
-                                INNER JOIN `training` ON `training.id_training` = `tie.id_user` 
-                WHERE `users.id_user` = `host.id_user` ";
-        $exec = (Dao::getConnexion())->prepare($sql);
+                                INNER JOIN `training` ON `training.id_training` = `tie.id_training` 
+                WHERE `users.id_user` = `host.id_user` ");
         try {
-        $exec->execute();
+            $requete->execute();
+            while($donnees = $requete->fetch(PDO::FETCH_ASSOC))
+            {
+                $slotDate=$donnees['slotDate'];
+                $name=$donnees['name'];
+                $slotHour=$donnees['slotHour'];
+                $id_session=$donnees['id_session'];
+                $id_training=$donnees['id_training'];
+                $id_user=$donnees['id_user'];
+                $session = new Session();
+                $list[] = $session;
+            }
         }
         catch (PDOException $e) {
             throw new LisaeException("Erreur requête", 1);
         }
+        return $list;
     }
     
     // delete via son id
