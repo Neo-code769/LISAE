97d2356eb0861e082a4934935bc7c3da11dcdb95
EmmaS <emmaschurrer@msn.com>
2020-06-04 16:24:46 +0200

    get list activity

--- model/recurringActivity.class.php
+++ model/recurringActivity.class.php
@@ -1,7 +1,10 @@
 <?php
 
 class RecurringActivity extends Activity{
-
-
-    
+    private $_idTheme;
+    function __construct($idTheme, $idActivity, $name, $description, $detailedDescription, $minNumberPerson, $maxNumberPerson, $registrationDeadline, $unsubscribeDeadline)
+    {
+        $this->_idTheme= $idTheme;
+        parent::__construct($idActivity,$name, $description, $detailedDescription, $minNumberPerson, $maxNumberPerson, $registrationDeadline, $unsubscribeDeadline);
+    }
 }
