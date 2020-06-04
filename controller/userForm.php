75816a073a689757163c7204f2ca65537bff40ba
NeoCode <pierre.trublereau@gmail.com>
2020-06-04 10:01:44 +0200

    change port

--- controller/userForm.php
+++ controller/userForm.php
@@ -67,7 +67,7 @@ class UserForm{
             $mail->addAddress($_POST['mail']);  // Personnaliser l'adresse d'envoi  
             $mail->addReplyTo('contact.afpa.lisae@gmail.com', 'Information'); // L'adresse de réponse
             $mail->Subject = 'Confirmation de votre Mail - AFPA-LISAE';
-            $link = "http://www.lisae.fr/view/registration/confirm-registration.php?mail=" . $this->_mail; //script".'?verification_code='.urlencode($user_activation_hash); // verification code exemple
+            $link = "http://www.lisae.fr:8081/view/registration/confirm-registration.php?mail=" . $this->_mail; //script".'?verification_code='.urlencode($user_activation_hash); // verification code exemple
             $mail->Body = "Veuillez confirmer votre adresse en mail en cliquant sur ce lien:<br><br>". ' '.$link; // Creation page: "LISAE/registration/confirm-registration"
             $mail->isHTML(true);
             $mail->setLanguage('fr');
