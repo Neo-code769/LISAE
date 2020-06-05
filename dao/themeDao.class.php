cb3dac7d3b96675bfe73cfddac8dd85b0e772644
EmmaS <emmaschurrer@msn.com>
2020-06-05 08:25:28 +0200

    fonction abstraite themeDao

--- dao/themeDao.class.php
+++ dao/themeDao.class.php
@@ -29,7 +29,30 @@ class themeDao extends Dao {
                 throw new LisaeException("Erreur", 1);
             }
     }
+    public function getList(): array
+    {
 
+    }		
+    
+    public function get(int $id) : array
+    {
+
+    }	
+    
+    public function insert($obj) : void	
+    {
+
+    }
+    // delete via son id
+    public function delete(int $id )
+    {
+
+    }
+    // update d'un objet
+    public function update($obj ) 
+    {
+
+    }
 }
 
 ?>
