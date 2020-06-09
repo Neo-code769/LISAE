e379d3f1adc76c841996d794e18854b7c452056a
Nale974 <58567101+Nale974@users.noreply.github.com>
2020-06-09 17:04:24 +0200

    galère tri

--- view/Collaborator/CollabView.class.php
+++ view/Collaborator/CollabView.class.php
@@ -21,22 +21,46 @@ class CollabView extends LisaeTemplateConnected {
     }
 
     public function setTheme($themeList){
-        //TEST
-        /* $result = var_dump($themeList);
-        foreach ($themeList as $theme) {
-            var_dump($theme->get_activity());
-        }  */
 
-        //APP
-        $result = "";
+        $arr = [];
         foreach ($themeList as $theme) {
             foreach ($theme->get_activity() as $activity) {
                 foreach($activity->get_slot() as $slot){
-                    $result .="<div class='eloce' style='background-color:".$theme->get_color()."'>".$slot->get_slotDateTimeStart()."-".$slot->get_slotDateTimeEnd()." - ".$theme->get_name()." - ".$activity->get_name()."</div>";
+                    $arr[]=["color" => $theme->get_color(),
+                    "dts" => $slot->get_slotDateTimeStart(),
+                    "dte" => $slot->get_slotDateTimeEnd(),
+                    "nTheme" => $theme->get_name(),
+                    "nActivity" => $activity->get_name()];
                 }
             }
+        } 
+
+        $data = array(
+            array(
+                "title" => "Another title",
+                "date"  => "Fri, 17 Jun 2011 08:55:57 +0200"
+            ),
+            array(
+                "title" => "My title",
+                "date"  => "Mon, 16 Jun 2010 06:55:57 +0200"
+            )
+        );
+
+        function date_sort($a, $b) {
+            return strtotime($a["dts"]) - strtotime($b["dts"]);
         }
-        $this->_eloce = $result;
+        usort($arr, "date_sort");
+        
+        var_dump(strftime('%A %d %B %Y %H:%M', strtotime($arr[1]["dts"])));
+        
+            //setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
+            //strftime('%A %d %B %Y %H:%M', strtotime($y["dts"]));
+        
+        //var_dump($arr);
+
+        /* $result .="<div class='eloce' style='background-color:".$theme->get_color()."'>".$slot->get_slotDateTimeStart()."-".$slot->get_slotDateTimeEnd()." - ".$theme->get_name()." - ".$activity->get_name()."</div>"; */
+
+        //$this->_eloce = $result;
     }
 
     public function setInfoUser($info){
