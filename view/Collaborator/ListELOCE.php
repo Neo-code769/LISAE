39065bbe4f6b84af2697eff58872072a1d79fd96
NeoCode <pierre.trublereau@gmail.com>
2020-06-09 16:09:27 +0200

    fixed link ListELOCE

--- view/Collaborator/ListELOCE.php
+++ view/Collaborator/ListELOCE.php
@@ -4,11 +4,14 @@
     </div>
 
     <nav id="navigation" class="container-fluid">
-        <div>
-            <button id="link">Thémes Activité</button>
+        <div id="link">
+            <a href="../collab/softskill" style="text-decoration:none">Soft Skills</a>
         </div>
-        <div>
-            <button id="link" a href="">Tableau de Bord</button>
+        <div id="link">
+            <a href="../collab/jobcible" style="text-decoration:none">Job Cible 2.0</a>
+        </div>
+        <div id="link">
+            <a href="./dashboard" style="text-decoration:none">Tableau de Bord</a>
         </div>
     </nav>
 
