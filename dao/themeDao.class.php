b621bf15494259debdc7a91773764d5d30c62de5
NeoCode <pierre.trublereau@gmail.com>
2020-06-24 15:40:24 +0200

    ++

--- dao/themeDao.class.php
+++ dao/themeDao.class.php
@@ -347,14 +347,14 @@ class ThemeDao extends Dao {
 
     // requete de désinscription d'un collaborateur à un créneau d'activité
 
-    public function deregistrationSlot($id_user,$id_session,$idActivity, $idSlot)    
+    public function deregistrationSlot($id_user,$id_session,$idActivity, $idSlot)       //TOFIX
     {
         $sql = "DELETE FROM `participate`
         WHERE `participate`.`id_user` = $id_user 
         AND `participate`.`id_activity` = $idActivity 
-        AND `participate`.`id_session` = $id_session     
+        AND `participate`.`id_session` = $id_session       
         AND `participate`.`slotDateStart` = (SELECT slotDateStart from host WHERE id_slot = $idSlot)";
-        var_dump($sql);
+        //var_dump($sql);
         $exec = (Dao::getConnexion())->prepare($sql);
         try{
             $exec->execute();
